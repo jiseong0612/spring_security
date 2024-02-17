73519adbcc17931054aede1a151914d7d1600d5f
jiseong0612 <hth5545@gmail.com>
2024-02-17 21:10:22 +0900

    뷰 cors 접근 테스트  성공

--- security3/src/main/java/com/example/demo/controller/IndexController.java
+++ security3/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,24 +1,38 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequestMapping("/api")
 public class IndexController {
+
 	@GetMapping("/")
+	@ResponseBody
 	public String index() {
 		log.debug("test~~~~~");
+
 		return "index";
 	}
 
 	@GetMapping("/login")
 	@ResponseBody
-	public String login() {
-		return "login";
+	public ResponseEntity<Map<String, Object>> login(@RequestParam Map<String, Object> inMap) {
+		log.debug("inMap = {}", inMap);
+		
+		return new ResponseEntity<Map<String, Object>>(inMap, HttpStatus.OK);
 	}
 
 	@GetMapping("/logout")
@@ -43,6 +57,14 @@ public class IndexController {
 	@ResponseBody
 	public String admin() {
 		return "admin";
+	}
+
+	@PostMapping("/register")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> getMethodName(@RequestBody Map<String, Object> inMap) {
+		log.debug("inMap = {}", inMap);
+
+		return new ResponseEntity<Map<String, Object>>(inMap, HttpStatus.OK);
 	}
 
 }
