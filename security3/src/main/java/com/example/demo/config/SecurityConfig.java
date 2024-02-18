b901b87454c970cffa5d5f4b421c6128f0bebd00
jiseong0612 <hth5545@gmail.com>
2024-02-18 11:41:57 +0900

    회원가입시 비밀번호 암호화

--- security3/src/main/java/com/example/demo/config/SecurityConfig.java
+++ security3/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,19 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
+//	@Bean
+//	public WebSecurityCustomizer webSecurityCustomizer() {
+//		return (web) -> web.ignoring()
+//				.antMatchers("/resources/**", "/favicon.ico");
+//	}
 	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring()
-				.antMatchers("/resources/**", "/favicon.ico");
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
 	}
-
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authorize -> authorize
